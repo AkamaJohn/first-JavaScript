d352068fb4dff903ca934ea5267a0560cba0638d
john <johnakama3@gmail.com>
2023-08-10 16:15:48 +0300

    my first commit

---
+++ main .js
@@ -0,0 +1,15 @@
+// // this where your javascript code will go
+// alert("hello world");
+// alert("am learning javascript");
+// alert("love coding")
+
+document.getElementById("test").innerHTML = "This is the assignment from javascript";
+
+function onlineClass() {
+    document.getElementById("test").innerHTML = "I love javascript its a very interenting topic to learn ";
+}
+document.getElementById("class").innerHTML = "want to become the guru in software development so help me God";
+function software() {
+
+    document.getElementById("class").innerHTML = "We are moving towards the digital word ";
+}
